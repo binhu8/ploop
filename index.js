2e8d27734f5bfbab1340f497e0a0d8256469e9b4
Fabio Silva <binhudo8@gmail.com>
2023-05-26 18:18:12 -0300

    cors 1

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const database = require("./src/database/database.js");
 const cors = require('cors')
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://www.ploop.netlify.app'
-}))
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.get('/', (req, res)=>{
     res.send('ploop  api')
